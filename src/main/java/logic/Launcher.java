19433e5fa1fa18a46ada28363deaba3a43b61e67
janba <jan.bauer1@icloud.com>
2022-10-29 00:31:49 +0200

    Random emotes function

--- src/main/java/logic/Launcher.java
+++ src/main/java/logic/Launcher.java
@@ -9,11 +9,12 @@ import net.dv8tion.jda.api.hooks.InterfacedEventManager;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.jetbrains.annotations.NotNull;
+import java.util.Random;
 
 public class Launcher extends ListenerAdapter {
     private final static InterfacedEventManager manager = new InterfacedEventManager();
     public static JDA jda;
-    private final static String TOKEN = "token";
+    private final static String TOKEN = "ODM1MjcwNDA0OTM5NDQ4MzMy.GyFGlV._RfQB6tlRCwSL9og6CyqPvo3vP4izAy4TnDspA";
 
     private  final static String PREFIX = "~";
 
@@ -42,11 +43,15 @@ public class Launcher extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         System.out.println("nachricht!");
+        String[] emotes = {"Passt auf, wo ihr hin tretet!", "Hallo, wie gehts denn so ?", "Das ist , als würde man in einem See voller Giftmilben baden.", "Ihr seid am Zug!", "Die beste Verteidigung ist ein schneller und entschlossener Angriff!", "Nur ein Sith kennt nix als extreme", "anakin?! was machst du hier?? du solltest dich doch auf coruscant melden!!", "naja, das hier bereitet mir weniger angst als das essen im jedi tempel!", "Immer noch besser als mehr Gandalf", "Ihr seid mehr Maschine als Mensch"};
+        Random random = new Random();
+        int rand = random.nextInt(emotes.length);
+
         String[] args = event.getMessage().getContentRaw().split("\\s+");
         String command = args[0].toLowerCase().replaceFirst(PREFIX, "");
         if (event.getMessage().getContentRaw().startsWith(PREFIX)) {
-            if(command.equalsIgnoreCase("ping")){
-                event.getMessage().reply("Pong!").queue();
+            if(command.equalsIgnoreCase("rand")){
+                event.getMessage().reply(emotes[rand]).queue();
             }
 
         }
